7f64fab2098dbd4b295218211f577f29f2cb2b84
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-03-11 15:51:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+### **The "Hangman" game in console using ruby**
+Command line based version of "Hangman"
+
+Introduction
+In this game, we have to figure out a secret word by guessing one letter at a time.
+However, with each error, the man will get one step closer to death!
+The goal is to successfully guess the word before the man gets hanged.
+___
+To play the game you need to execute following commands in your command line.
+
+```
+bundle intsall
+```
+
+```
+bundle exec ruby main.rb
+```
+
+To add new words to the game you need to get in `data/words.txt` and add
+your word to the list
---
+++ lib/console_interface.rb
@@ -0,0 +1,52 @@
+require 'colorize'
+
+class ConsoleInterface
+  FIGURES =
+      Dir["#{__dir__}/../data/figures/*.txt"].
+      sort.
+      map { |file_name| File.read(file_name) }
+
+  def initialize(game)
+    @game = game
+  end
+
+  def print_out
+    puts
+    puts "Слово: #{word_to_show}".colorize(:blue)
+    puts figure.colorize(:yellow)
+    puts "Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)
+    puts "У вас осталось ошибок: #{@game.errors_allowed}"
+
+    if @game.won?
+      puts "Поздравляем, вы выиграли!"
+    elsif @game.lost?
+      puts "Вы проиграли, загаданное слово: #{@game.word}"
+    end
+  end
+
+  def figure
+    FIGURES[@game.errors_made]
+  end
+
+  def word_to_show
+    result =
+      @game.letters_to_guess.map do |letter|
+        if letter == nil
+          "__"
+        else
+          letter
+        end
+      end
+
+    result.join(' ')
+  end
+
+  def errors_to_show
+    @game.errors.join(', ')
+  end
+
+  def get_input
+    print "Введите следующую букву: "
+    gets[0].upcase
+  end
+end
